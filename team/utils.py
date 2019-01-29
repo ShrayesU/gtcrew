f8c99f0288475661367d599b136d6ad8bbbe869d
Aleccc <alec.kaye@gmail.com>
2019-01-29 16:21:31 -0500

    Create utils.py

---
+++ team/utils.py
@@ -0,0 +1,26 @@
+STUDENT = 'student'
+COACH = 'coach'
+ALUMNI = 'alumni'
+HELD_BY_CHOICES = (
+    (STUDENT, 'Student'),
+    (COACH, 'Coach'),
+    (ALUMNI, 'Alumni'),
+)
+
+FALL = 'FALL'
+SPRING = 'SPRING'
+SEMESTER_CHOICES = (
+    (FALL, 'Fall'),
+    (SPRING, 'Spring'),
+)
+
+DEFAULT = 'BASE'
+HOME = 'HOME'
+ABOUT = 'ABOUT'
+TEAM = 'TEAM'
+TEMPLATE_CHOICES = (
+    (DEFAULT, 'Regular'),
+    (HOME, 'Home'),
+    (ABOUT, 'About'),
+    (TEAM, 'Team'),
+)
