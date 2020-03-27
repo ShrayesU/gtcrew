318e2323fd2877e93f10b19697e702b877c6eff6
alec <alec.kaye@gmail.com>
2020-03-27 01:00:28 -0400

    add staging as ALLOWED_HOSTS

--- rowingcrm/settings.py
+++ rowingcrm/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'gtcrew.herokuapp.com', 'app.gtcrew.com', 'gtcrew.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'gtcrew-staging.herokuapp.com',
+                 'gtcrew.herokuapp.com', 'app.gtcrew.com', 'gtcrew.com']
 
 # Application definition
 
