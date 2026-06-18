7902d61c2b2a258cb0d5fe27aecfaedcccbf37a6
Shrayes Upadhyayula <87687114+ShrayesU@users.noreply.github.com>
2026-06-17 21:56:03 -0400

    Make optional integrations safe for local preview

--- rowingcrm/settings.py
+++ rowingcrm/settings.py
@@ -13,25 +13,21 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 import dj_database_url
-import django_heroku
 from decouple import config
+
+try:
+    import django_heroku
+except ImportError:
+    django_heroku = None
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY', '')
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-# Domains and Hosts
 DOMAIN = os.environ.get('DOMAIN', '')
-
 DOMAIN_ALIASES = [
     d.strip()
     for d in os.environ.get('DOMAIN_ALIASES', '').split(',')
@@ -43,10 +39,8 @@ ALLOWED_HOSTS = [DOMAIN] + DOMAIN_ALIASES
 SECURE_SSL_REDIRECT = config('SECURE_SSL_REDIRECT', default=True, cast=bool)
 SECURE_SSL_HOST = DOMAIN
 
-# Application definition
-
 INSTALLED_APPS = [
-    'gtcrew',  # location of the Wagtail Page models
+    'gtcrew',
     'roster',
     'person',
     'award',
@@ -72,14 +66,9 @@ INSTALLED_APPS = [
     'wagtail.admin',
     'wagtail',
 
-    'wagtailcaptcha',
     'wagtailautocomplete',
-    'robots',
-    # 'wagalytics',
-
     'taggit',
     'modelcluster',
-
     'dal',
     'dal_select2',
     'django.contrib.admin',
@@ -93,11 +82,8 @@ INSTALLED_APPS = [
     'storages',
     'captcha',
     'django_cleanup',
-    'cuser',
     'django_summernote',
     'tempus_dominus',
-    'actstream',
-
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -111,7 +97,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'cuser.middleware.CuserMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
 
@@ -128,7 +113,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                # allow access to wagtail settings in templates
                 'wagtail.contrib.settings.context_processors.settings',
             ],
         },
@@ -137,66 +121,33 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'rowingcrm.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASE_URL = os.environ.get('DATABASE_URL', '')
 if DATABASE_URL != '':
     DATABASES = {'default': dj_database_url.parse(DATABASE_URL)}
 
-# Password validation
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-# Authentication Backends
 AUTHENTICATION_BACKENDS = (
-    # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
-
-    # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-# Internationalization
-# https://docs.djangoproject.com/en/2.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'America/New_York'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-# Default auto field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Django-RECAPTCHA
 RECAPTCHA_PUBLIC_KEY = config('RECAPTCHA_SITEKEY', '')
 RECAPTCHA_PRIVATE_KEY = config('RECAPTCHA_SECRETKEY', '')
-# SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']  # used during testing only
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(PROJECT_DIR, 'static')]
 
 if config('USE_S3', default=False, cast=bool):
     AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', '')
@@ -204,39 +155,25 @@ if config('USE_S3', default=False, cast=bool):
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', '')
     AWS_S3_CUSTOM_DOMAIN = config('AWS_S3_CUSTOM_DOMAIN', '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME)
     AWS_S3_ENDPOINT_URL = config('AWS_S3_ENDPOINT_URL', None)
-
-    AWS_S3_OBJECT_PARAMETERS = {
-        'CacheControl': 'max-age=86400',
-    }
-
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     AWS_LOCATION = 'static'
     STATICFILES_STORAGE = 'rowingcrm.storage_backends.StaticStorage'
     STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-
     DEFAULT_FILE_STORAGE = 'rowingcrm.storage_backends.MediaStorage'
-
     AWS_DEFAULT_ACL = 'private'
 else:
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATIC_URL = '/static/'
-
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     MEDIA_URL = '/media/'
 
-# SummerNote
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 SUMMERNOTE_THEME = 'bs4'
-SUMMERNOTE_CONFIG = {
-    # 'iframe': False,
-    'attachment_require_authentication': True,
-}
+SUMMERNOTE_CONFIG = {'attachment_require_authentication': True}
 
-# Activity Stream
 SITE_ID = 1
 
-# Email Backend
 DEFAULT_FROM_EMAIL = os.getenv('FROM_EMAIL', 'webmaster@gtcrew.com')
 SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY', '')
 if SENDGRID_API_KEY:
@@ -244,7 +181,6 @@ if SENDGRID_API_KEY:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-# Wagtail
 WAGTAIL_SITE_NAME = 'Georgia Tech Rowing'
 WAGTAILEMBEDS_RESPONSIVE_HTML = True
 WAGTAILADMIN_GLOBAL_EDIT_LOCK = True
@@ -259,37 +195,24 @@ WAGTAILADMIN_RICH_TEXT_EDITORS = {
         }
     },
 }
-
-# Base URL to use when referring to full URLs within the Wagtail admin backend -
-# e.g. in notification emails. Don't include '/admin' or a trailing slash
 WAGTAILADMIN_BASE_URL = DOMAIN
 
-# Import local_settings if local
 try:
     from .local_settings import *
 except ImportError:
     pass
 
-# Google Analytics
 GOOGLE_ANALYTICS_CODE = config('GOOGLE_ANALYTICS_CODE', '')
 GA_VIEW_ID = config('GA_VIEW_ID', '')
 GA_KEY_CONTENT = config('GA_KEY_CONTENT', '')
 
-# Login
 LOGIN_URL = WAGTAIL_FRONTEND_LOGIN_URL
-
-# where requests are redirected after login when the LoginView doesn't get a next GET parameter.
 LOGIN_REDIRECT_URL = 'wagtailadmin_home'
+PUBLIC_GROUP_ID = 2
 
-PUBLIC_GROUP_ID = 2  # editors
-
-# Allauth settings
 ACCOUNT_FORMS = {'signup': 'gtcrew.forms.AllauthSignupForm'}
 ACCOUNT_AUTHENTICATION_METHOD = "username_email"
-# ACCOUNT_CONFIRM_EMAIL_ON_GET = True
 ACCOUNT_EMAIL_REQUIRED = True
-# ACCOUNT_EMAIL_VERIFICATION = "mandatory"
-# ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = True
 ACCOUNT_LOGIN_ON_PASSWORD_RESET = True
 ACCOUNT_LOGOUT_REDIRECT_URL = '/'
 ACCOUNT_PRESERVE_USERNAME_CASING = False
@@ -297,6 +220,5 @@ ACCOUNT_SESSION_REMEMBER = True
 ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = True
 ACCOUNT_USERNAME_BLACKLIST = ["admin", "god", "gtcrew"]
 
-# Activate Django-Heroku.
-if not DEBUG:
+if not DEBUG and django_heroku:
     django_heroku.settings(locals(), staticfiles=False)
